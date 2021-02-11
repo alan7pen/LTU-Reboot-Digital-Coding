512111b9717dcf42022b110b1f93b1deb843ccf3
alan7pen <77551639+alan7pen@users.noreply.github.com>
2021-02-11 20:49:30 +0000

    Update task-3.1.4.js
    sheet width/height
    calculate area

---
+++ module-3/lesson-1/scripts/task-3.1.4.js
@@ -0,0 +1,11 @@
+var sheetWidth = 640; // mm
+var sheetHeight = 720; // mm
+var sheetArea = sheetWidth * sheetHeight;
+
+
+console.log('Width (mm)=', sheetWidth);
+console.log('Height (mm)=', sheetHeight)
+console.log('Sheet Area (mm) = ' + sheetArea);
+
+
+
