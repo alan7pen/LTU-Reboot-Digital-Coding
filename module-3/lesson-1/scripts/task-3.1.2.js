57d045814ab73276487930af3a2fc550bf197d09
alan7pen <77551639+alan7pen@users.noreply.github.com>
2021-02-11 20:10:16 +0000

    Update task-3.1.2.js
    comments - single/multi line
    document.write

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,12 @@
+/*
+  this is a
+  multi line
+  comment
+*/
+
+// this is a single line comment
+
+alert('Hello from the alert box'); // this popus up with an alert message
+console.log('Hello from the console'); // shows a message in the console
+document.write('Hello from the document.write') // prints message to web page
+
