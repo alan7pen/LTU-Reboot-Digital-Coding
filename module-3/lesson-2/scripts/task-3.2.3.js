155f24b582698f6ba234df9d21e214008238fc7c
alan7pen <77551639+alan7pen@users.noreply.github.com>
2021-02-16 19:52:28 +0000

    Update task-3.2.3.js
    function with return

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -1,14 +1,28 @@
 /* EXAMPLE */
 
+
 function greet(greeting, name) {
   return greeting + ' ' + name;
 }
 
-var message = greet('Hello', 'Luke');
-    console.log( message ); // Output: Hello Luke
+function fullName(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
 
+
+var message = greet('Hello', 'Luke');
+console.log( message ); // Output: Hello Luke
+
+
+var person1 = fullName('Fred', 'Blogs');
+var person2 = fullName('Mary', 'Bobbin');
+console.log(person1);
+console.log(person2);
 /* EXAMPLE */
 
+
+console.log('person1 variable = ' + person1);
+console.log(greet('How do', person2));
 // ADD YOUR CODE BELOW HERE
 
 // ADD YOUR CODE ABOVE HERE
