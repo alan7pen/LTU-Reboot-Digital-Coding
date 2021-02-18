4baacec4e355d6ebf682d9026e67f57668bfe76e
alan7pen <77551639+alan7pen@users.noreply.github.com>
2021-02-18 19:13:01 +0000

    Update task-3.3.1.js
    Percentage calculator function

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,21 @@
+/*
+  1. Create a function named percentageCalculator
+
+  2. Add 2 arguments for “number” and “percentage”
+
+  3. Do the maths required to work out a percentage with the arguments
+
+  4. Return the result of the maths
+
+  5. Console.log the returned value
+
+  6. Commit your changes
+
+  7. Push your changes to remote
+  */
+
+  function percentageCalculator(number, percentage) {
+    return number * (percentage / 100);
+  }
+
+  console.log(percentageCalculator(30, 35));
