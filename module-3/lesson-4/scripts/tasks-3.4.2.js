5b579e84cb2a6d5c77a6fd2191ec8c063bc7a244
alan7pen <77551639+alan7pen@users.noreply.github.com>
2021-02-23 19:59:25 +0000

    Create tasks-3.4.2.js
    Array to show favourite foods

---
+++ module-3/lesson-4/scripts/tasks-3.4.2.js
@@ -0,0 +1,12 @@
+let favouriteFoods = [
+  'Cheese', 'Ham', 'Bread', 'Beans'
+];
+
+maxItems = favouriteFoods.length;
+
+console.log(`${maxItems} items of food`);
+console.log('----------------------------------------');
+
+for (let loop = 0; loop < maxItems; loop++) {
+  console.log(favouriteFoods[loop]);
+}
