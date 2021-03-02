ea5dc4b2395ac1e5355531c8193ab8a7638b7dd0
alan7pen <77551639+alan7pen@users.noreply.github.com>
2021-03-02 20:45:04 +0000

    Create task-3.6.4.js
    addEventListner
    event.preventDefault();

---
+++ module-3/lesson-6/scripts/task-3.6.4.js
@@ -0,0 +1,16 @@
+//console.log('yyy');
+
+
+
+let googleLink = document.querySelector('a');
+
+
+googleLink.addEventListener('click', function(event) {
+  
+  event.preventDefault();
+  //console.log(event);
+
+  alert('link disabled');
+  
+  //alert('xxx');
+});
